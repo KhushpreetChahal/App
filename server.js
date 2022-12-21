a090483a10ec63f18e1215b29bd24782a90f09b6
Tracer786 <msingh6_be20@thapar.edu>
2022-12-21 17:20:36 +0530

    server.js updated

--- server.js
+++ server.js
@@ -7,3 +7,5 @@ app.listen(PORT, () => {
 
 // to run the above file run the command "node server.js"
 // but the problem here is that everytime we change the content of the file we have to run the command 'node server.js' again and again
+
+// now to run the server we just need to give the command as 'yarn serve' which is defined in package.json 
